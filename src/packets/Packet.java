6768c4d900eadf30cb39dccb2336aac3d1512722
Parnpreet Kanwar <parnpreet.kanwar@uleth.ca>
2021-03-24 13:33:48 -0600

    Dealt with the CRC32 and

--- src/packets/Packet.java
+++ src/packets/Packet.java
@@ -2,114 +2,128 @@ package packets;
 
 import com.sun.istack.internal.Nullable;
 
+import java.lang.reflect.Array;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.ShortBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.Date;
 import java.util.zip.CRC32;
 
 public class Packet {
-    private byte type;
-    private byte tr;
-    private byte windows;
-    private byte sequenceNumber;
-    private byte length;
+    private int type;
+    private int tr;
+    private int windows;
+    private int sequenceNumber;
+    private int length;
     private byte[] payload;
-    private final byte HEADER = (byte)36;
-    private final byte PAYLOAD_AND_HEADER = (byte) 512 + HEADER;
+    private final int HEADER = 40;
+    private final int PAYLOAD_AND_HEADER =  512 + HEADER;
 
     public Packet() {
     }
 
     public Packet(int type, int tr, int windows, int sequenceNumber, int length, byte[] payload) {
-        this.type = (byte) type;
-        this.tr = (byte) tr;
-        this.windows = (byte) windows;
-        this.sequenceNumber = (byte) sequenceNumber;
-        this.length = (byte) length;
+        this.type =  type;
+        this.tr = tr;
+        this.windows =  windows;
+        this.sequenceNumber = sequenceNumber;
+        this.length =  length;
         this.payload = payload;
     }
 
-    public byte getType() {
+    public int getType() {
         return type;
     }
 
-    public void setType(byte type) {
-        this.type = type;
-    }
-
-    public byte getTr() {
+    public int getTr() {
         return tr;
     }
 
-    public void setTr(byte tr) {
-        this.tr = tr;
-    }
-
-    public byte getWindows() {
+    public int getWindows() {
         return windows;
     }
 
-    public void setWindows(byte windows) {
-        this.windows = windows;
-    }
-
-    public byte getSequenceNumber() {
+    public int getSequenceNumber() {
         return sequenceNumber;
     }
 
-    public void setSequenceNumber(byte sequenceNumber) {
-        this.sequenceNumber = sequenceNumber;
-    }
-
-    public byte getLength() {
+    public int getLength() {
         return length;
-    }
-
-    public void setLength(byte length) {
-        this.length = length;
     }
 
     public byte[] getPayload() {
         return payload;
     }
 
-    public void setPayload(byte[] payload) {
-        this.payload = payload;
-    }
-
     public static byte[] checksum(byte[] payload){
         CRC32 check = new CRC32();
         check.update(payload);
         String crcNumber = check.getValue()+"";
-        byte[] checkBytes = crcNumber.getBytes(StandardCharsets.UTF_8);
+        byte[] checkBytes = ByteBuffer.allocate(4).putLong(check.getValue()).array();
+//        byte [] checkBytes = crcNumber.getBytes(StandardCharsets.UTF_8);
         return checkBytes;
     }
-    public ByteBuffer tBu() {
-        ByteBuffer buffer = ByteBuffer.allocate(PAYLOAD_AND_HEADER).order(ByteOrder.BIG_ENDIAN);
-        write(buffer);
-        buffer.flip();
-        return buffer;
+
+    public static class PacketBuilder {
+        private int type;
+        private int tr;
+        private int windows;
+        private int sequenceNumber;
+        private int length;
+        private byte[] payload;
+        private final int HEADER = 40;
+        private final int PAYLOAD_AND_HEADER =  512 + HEADER;
+
+
+        public PacketBuilder setType(int type) {
+            this.type = (int) Integer.toUnsignedLong(type);
+            return this;
+        }
+
+        public PacketBuilder setTr(int tr) {
+            this.tr = (byte) Integer.toUnsignedLong(tr);
+            return this;
+        }
+
+        public PacketBuilder setWindows(int windows) {
+            this.windows = (byte) Integer.toUnsignedLong(windows);
+            return this;
+        }
+
+
+        public PacketBuilder setSequenceNumber(int sequenceNumber) {
+            this.sequenceNumber = (byte) Integer.toUnsignedLong(sequenceNumber);
+            return this;
+        }
+
+        public PacketBuilder setLength(int length) {
+            this.length = (byte)Integer.toUnsignedLong(length);
+            return this;
+        }
+
+
+        public PacketBuilder setPayload(byte[] payload) {
+            this.payload = payload;
+            return this;
+        }
+
+        public Packet createPack() {
+            return new Packet(type, tr, windows, sequenceNumber, length, payload);
+        }
+
     }
 
-    private void write (ByteBuffer buf) {
-        Date date = new Date();
-        buf.put(type);
-        buf.put(tr);
-        buf.put(windows);
-        buf.put(sequenceNumber);
-        buf.put(length);
-        buf.putLong(date.getTime());
-        buf.put(checksum(payload))   ;
-        buf.put(payload);
-    }
     public static void main(String[] args) {
-        byte bu[] = {0,1,0,1};
-        Packet pa = new Packet(1,0,16,1,56,bu);
-     ByteBuffer ra = pa.tBu();
-        for (int i = 0; i < ra.limit() ; i++) {
+        Packet pa = new Packet.PacketBuilder().setType(0x2002).setTr(0).setWindows(12).setSequenceNumber(1).setLength(22)
+                .setPayload(null).createPack();
+        ArrayList<Packet> na = new ArrayList<>();
+        na.add(pa);
+        for (int i = 0; i < na.size(); i++) {
+            System.out.println( na.get(i).getType() );
 
-            System.out.println(ra.get(i) + "----------------"+ i);
         }
     }
 }
